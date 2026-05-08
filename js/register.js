31bd16195ff97aec8d200a822bdd3179024e66c5
Anastasia <anastasiabesleu@gmail.com>
2026-05-08 13:16:09 +0300

    Fix bugs on the Register page

--- js/register.js
+++ js/register.js
@@ -11,3 +11,55 @@ document.getElementById("registerForm").addEventListener("submit", function(e) {
 
   alert("Account created successfully 🚀");
 });
+
+
+const html = document.documentElement;
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const thumb = document.getElementById("themeThumb");
+
+    // =========================
+    // LOAD SAVED THEME
+    // =========================
+
+    const savedTheme = localStorage.getItem("theme") || "dark";
+
+    html.setAttribute("data-theme", savedTheme);
+
+    updateThemeIcon(savedTheme);
+
+    // =========================
+    // TOGGLE THEME
+    // =========================
+
+    window.toggleTheme = function () {
+
+        const currentTheme = html.getAttribute("data-theme");
+
+        const newTheme =
+            currentTheme === "dark"
+                ? "light"
+                : "dark";
+
+        html.setAttribute("data-theme", newTheme);
+
+        localStorage.setItem("theme", newTheme);
+
+        updateThemeIcon(newTheme);
+    };
+
+    // =========================
+    // ICON UPDATE
+    // =========================
+
+    function updateThemeIcon(theme) {
+
+        if (!thumb) return;
+
+        thumb.textContent =
+            theme === "dark"
+                ? "☀️"
+                : "🌙";
+    }
+});
