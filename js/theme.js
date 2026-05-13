691bb97e41668eeb391e516a3458fbcad48ca1b9
Anastasia <anastasiabesleu@gmail.com>
2026-05-13 12:24:27 +0300

    Fix text color in home page , hero

--- js/theme.js
+++ js/theme.js
@@ -17,6 +17,7 @@ html.classList.add("theme-preload");
 // =========================
 
 window.addEventListener("DOMContentLoaded", () => {
+    
 
     const thumb =
         document.getElementById("themeThumb");
